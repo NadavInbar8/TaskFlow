6f7de84b8ffceabe29bcc58a17d6d238156bf8cc
Oded Kovo <odedkovo2@gmail.com>
2022-01-18 19:37:46 +0200

    added header footer with color

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom ';
 
 export const Board = () => {
   return (
     <div>
+      <Link to='/carddetails/01'>to card details</Link>
       <h1>board</h1>
     </div>
   );
