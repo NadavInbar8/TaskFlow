572906026d9585b211533b69399ee043d4790474
NadavInbar8 <nadavfrc@gmail.com>
2022-01-18 20:26:55 +0200

    starting to work on the homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import logo from '../assets/imgs/icon.png';
 
 export const Homepage = () => {
   return (
     <div>
-      <h1>TaskFlow</h1>
+      <div className='homepage-header flex'>
+        <img src={logo} alt='logo' />
+        <h1>TaskFlow</h1>
+      </div>
     </div>
   );
 };
