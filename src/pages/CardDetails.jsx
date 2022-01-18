af01d2249dcddd8ea5edb09145d3c046b6f04a3d
Oded Kovo <odedkovo2@gmail.com>
2022-01-18 22:50:07 +0200

    added card details cmp

--- src/pages/CardDetails.jsx
+++ src/pages/CardDetails.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory, useParams } from 'react-router-dom';
 export function CardDetails() {
-	console.log('hello');
-	return (
-		<div>
-			<Link className='go-back-container' to='/board' />
-			<div className='card-details'>
-				<h1>card details</h1>
-			</div>
-		</div>
-	);
+  const [card, setCard] = useState(null);
+
+  const { id } = useParams();
+  console.log(id);
+  useEffect(() => {
+    // setCard(getCard());
+  }, []);
+
+  //   function getCard() {
+  //     const { id } = useParams();
+  //     console.log(id);
+  //   }
+
+  console.log('hello');
+  return (
+    <div>
+      <Link className='go-back-container' to='/board' />
+
+      <div className='card-details'>
+        <div className='card-details-top'>
+          <h1>card details</h1>
+          <Link to='/board'>
+            <button>X</button>
+          </Link>
+        </div>
+
+        <div className='card-details-main'></div>
+      </div>
+    </div>
+  );
 }
