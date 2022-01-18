eca274236451cd1a2be9c615c43867bc22c2d45d
Oded Kovo <odedkovo2@gmail.com>
2022-01-18 22:13:52 +0200

    added json board to storage

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 // import {CardDetails} from './CardDetails.jsx';
-import {Link, Route, Switch} from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 
 export class Board extends React.Component {
-	render() {
-		return (
-			<selection>
-				{/* <Route component={() => <CardDetails />} path='/board/carddetails' /> */}
+  render() {
+    return (
+      <selection>
+        {/* <Route component={() => <CardDetails />} path='/board/carddetails' /> */}
 
-				<Link to='/board/carddetails'>to card details</Link>
-				<div>
-					<h1>board</h1>
-				</div>
-			</selection>
-		);
-	}
+        <Link to='/board/carddetails'>to card details</Link>
+        <div>
+          <h1>board</h1>
+        </div>
+      </selection>
+    );
+  }
 }
---
+++ src/services/board.service.js
@@ -0,0 +1,245 @@
+import { storageService } from './async-storage.service';
+// import { httpService } from './http.service'
+// import { socketService, SOCKET_EVENT_USER_UPDATED } from './socket.service'
+const STORAGE_KEY_BOARD = 'board';
+// var gWatchedUser = null;
+
+export const BoardService = {
+  // login,
+  // logout,
+  // signup,
+  // getLoggedinUser,
+  getBoards,
+  // getById,
+  // remove,
+  // update,
+  // changeScore
+};
+
+// To help debugging from console
+// window.userService = userService;
+
+function getBoards() {
+  return storageService.query('board');
+  //   return httpService.get(`user`);
+}
+
+// async function getById(userId) {
+//     // const user = await storageService.get('user', userId)
+//     const user = await httpService.get(`user/${userId}`)
+//     gWatchedUser = user;
+//     return user;
+// }
+// function remove(userId) {
+//     // return storageService.remove('user', userId)
+//     return httpService.delete(`user/${userId}`)
+// }
+
+// async function update(user) {
+//     // await storageService.put('user', user)
+//     user = await httpService.put(`user/${user._id}`, user)
+//     // Handle case in which admin updates other user's details
+//     if (getLoggedinUser()._id === user._id) _saveLocalUser(user)
+//     return user;
+// }
+
+// async function login(userCred) {
+//     // const users = await storageService.query('user')
+//     // const user = users.find(user => user.username === userCred.username)
+//     // return _saveLocalUser(user)
+
+//     const user = await httpService.post('auth/login', userCred)
+//     // socketService.emit('set-user-socket', user._id);
+//     if (user) return _saveLocalUser(user)
+// }
+// async function signup(userCred) {
+//     // userCred.score = 10000;
+//     // const user = await storageService.post('user', userCred)
+//     const user = await httpService.post('auth/signup', userCred)
+//     // socketService.emit('set-user-socket', user._id);
+//     return _saveLocalUser(user)
+// }
+// async function logout() {
+//     sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER)
+//     // socketService.emit('unset-user-socket');
+//     return await httpService.post('auth/logout')
+// }
+
+// async function changeScore(by) {
+//     const user = getLoggedinUser()
+//     if (!user) throw new Error('Not loggedin')
+//     user.score = user.score + by || by
+//     await update(user)
+//     return user.score
+// }
+
+// function _saveLocalUser(user) {
+//     sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user))
+//     return user
+// }
+
+// function getLoggedinUser() {
+//     return JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN_USER) || 'null')
+// }
+
+// // (async ()=>{
+// //     await userService.signup({fullname: 'Puki Norma', username: 'user1', password:'123',score: 10000, isAdmin: false})
+// //     await userService.signup({fullname: 'Master Adminov', username: 'admin', password:'123', score: 10000, isAdmin: true})
+// //     await userService.signup({fullname: 'Muki G', username: 'muki', password:'123', score: 10000})
+// // })();
+
+// // This IIFE functions for Dev purposes
+// // It allows testing of real time updates (such as sockets) by listening to storage events
+// (async () => {
+//     // Dev Helper: Listens to when localStorage changes in OTHER browser
+
+//     // Here we are listening to changes for the watched user (comming from other browsers)
+//     window.addEventListener('storage', async () => {
+//         if (!gWatchedUser) return;
+//         const freshUsers = await storageService.query('user')
+//         const watchedUser = freshUsers.find(u => u._id === gWatchedUser._id)
+//         if (!watchedUser) return;
+//         if (gWatchedUser.score !== watchedUser.score) {
+//             console.log('Watched user score changed - localStorage updated from another browser')
+//             socketService.emit(SOCKET_EVENT_USER_UPDATED, watchedUser)
+//         }
+//         gWatchedUser = watchedUser
+//     })
+// })();
+
+// // This is relevant when backend is connected
+// // (async () => {
+// //     var user = getLoggedinUser()
+// //     if (user) socketService.emit('set-user-socket', user._id)
+// // })();
+
+const board = {
+  _id: 'b101',
+  title: 'Robot dev proj',
+  createdAt: 1589983468418,
+  createdBy: {
+    _id: 'u101',
+    fullname: 'Abi Abambi',
+    imgUrl: 'http://some-img',
+  },
+  style: {},
+  labels: [
+    {
+      id: 'l101',
+      title: 'Done',
+      color: '#61bd4f',
+    },
+  ],
+  members: [
+    {
+      _id: 'u101',
+      fullname: 'Tal Tarablus',
+      imgUrl: 'https://www.google.com',
+    },
+  ],
+  groups: [
+    {
+      id: 'g101',
+      title: 'Group 1',
+      tasks: [
+        {
+          id: 'c101',
+          title: 'Replace logo',
+        },
+        {
+          id: 'c102',
+          title: 'Add Samples',
+        },
+      ],
+      style: {},
+    },
+    {
+      id: 'g102',
+      title: 'Group 2',
+      tasks: [
+        {
+          id: 'c103',
+          title: 'Do that',
+        },
+        {
+          id: 'c104',
+          title: 'Help me',
+          status: 'in-progress',
+          description: 'description',
+          comments: [
+            {
+              id: 'ZdPnm',
+              txt: 'also @yaronb please CR this',
+              createdAt: 1590999817436.0,
+              byMember: {
+                _id: 'u101',
+                fullname: 'Tal Tarablus',
+                imgUrl:
+                  'http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg',
+              },
+            },
+          ],
+          checklists: [
+            {
+              id: 'YEhmF',
+              title: 'Checklist',
+              todos: [
+                {
+                  id: '212jX',
+                  title: 'To Do 1',
+                  isDone: false,
+                },
+              ],
+            },
+          ],
+          members: [
+            {
+              _id: 'u101',
+              username: 'Tal',
+              fullname: 'Tal Tarablus',
+              imgUrl:
+                'http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg',
+            },
+          ],
+          labelIds: ['l101', 'l102'],
+          createdAt: 1590999730348,
+          dueDate: 16156215211,
+          byMember: {
+            _id: 'u101',
+            username: 'Tal',
+            fullname: 'Tal Tarablus',
+            imgUrl:
+              'http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg',
+          },
+          style: {
+            bgColor: '#26de81',
+          },
+        },
+      ],
+      style: {},
+    },
+  ],
+  activities: [
+    {
+      id: 'a101',
+      txt: 'Changed Color',
+      createdAt: 154514,
+      byMember: {
+        _id: 'u101',
+        fullname: 'Abi Abambi',
+        imgUrl: 'http://some-img',
+      },
+      task: {
+        id: 'c101',
+        title: 'Replace Logo',
+      },
+    },
+  ],
+  // for monday
+  cmpsOrder: ['status-picker', 'member-picker', 'date-picker'],
+};
+
+putJsonInStorage();
+function putJsonInStorage() {
+  localStorage.setItem(STORAGE_KEY_BOARD, JSON.stringify(board));
+}
