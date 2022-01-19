d5ff318cd0b51b668437c95bd1c0596e54971bbd
Oded Kovo <odedkovo2@gmail.com>
2022-01-19 12:41:56 +0200

    added board action

--- src/store/board.reducer.js
+++ src/store/board.reducer.js
@@ -1,54 +1,56 @@
 const initialState = {
-	boards: [],
-	filterBy: null,
-	isModalShown: false,
+  boards: [],
+  // filterBy: null,
+  // isModalShown: false,
 };
 
 export function boardReducer(state = initialState, action) {
-	let newState = state;
-	switch (action.type) {
-		case 'SET_BOARDS':
-			newState = {...state, boards: [...action.boards]};
-			break;
+  let newState = state;
+  switch (action.type) {
+    case 'SET_BOARDS':
+      newState = { ...state, boards: [...action.boards] };
+      break;
 
-		case 'ADD_BOARD':
-			newState = {...state, boards: [...state.boards, action.savedBoard]};
-			break;
+    case 'ADD_BOARD':
+      newState = { ...state, boards: [...state.boards, action.savedBoard] };
+      break;
 
-		case 'REMOVE_BOARD':
-			newState = {
-				...state,
-				boards: state.boards.filter((board) => board._id !== action.boardId),
-			};
-			break;
+    case 'REMOVE_BOARD':
+      newState = {
+        ...state,
+        boards: state.boards.filter((board) => board._id !== action.boardId),
+      };
+      break;
 
-		case 'UPDATE_BOARD':
-			newState = {
-				...state,
-				boards: state.boards.map((board) => {
-					console.log(board, action.savedBoard);
-					return board._id === action.savedBoard._id ? action.savedBoard : board;
-				}),
-			};
-			break;
+    case 'UPDATE_BOARD':
+      newState = {
+        ...state,
+        boards: state.boards.map((board) => {
+          console.log(board, action.savedBoard);
+          return board._id === action.savedBoard._id
+            ? action.savedBoard
+            : board;
+        }),
+      };
+      break;
 
-		case 'SET_FILTER_BY':
-			newState = {
-				...state,
-				filterBy: action.filterBy,
-			};
-			break;
+    case 'SET_FILTER_BY':
+      newState = {
+        ...state,
+        filterBy: action.filterBy,
+      };
+      break;
 
-		case 'TOGGLE_MODAL':
-			console.log('in reducer toggling');
-			newState = {
-				...state,
-				isModalShown: !state.isModalShown,
-			};
-			break;
+    case 'TOGGLE_MODAL':
+      console.log('in reducer toggling');
+      newState = {
+        ...state,
+        isModalShown: !state.isModalShown,
+      };
+      break;
 
-		default:
-	}
+    default:
+  }
 
-	return newState;
+  return newState;
 }
