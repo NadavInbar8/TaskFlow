4ec21878218c0bb58205f246004e43438a37b11f
Oded Kovo <odedkovo2@gmail.com>
2022-01-24 13:59:57 +0200

    added login page and signup page

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
-import {Homepage} from './pages/Homepage.jsx';
-import {WorkSpace} from './pages/WorkSpace.jsx';
-import {Board} from './pages/Board.jsx';
-
+import { Homepage } from './pages/Homepage.jsx';
+import { WorkSpace } from './pages/WorkSpace.jsx';
+import { Board } from './pages/Board.jsx';
+import { Login } from './pages/Login.jsx';
+import { Signup } from './pages/Signup.jsx';
 const routes = [
-	{path: '/workspace', component: WorkSpace, exact: true},
-	{path: '/board/:boardId', component: Board, exact: false},
-	{path: '/', component: Homepage, exact: true},
+  { path: '/workspace', component: WorkSpace, exact: true },
+  { path: '/board/:boardId', component: Board, exact: false },
+  { path: '/login', component: Login, exact: true },
+  { path: '/signup', component: Signup, exact: true },
+  { path: '/', component: Homepage, exact: true },
 ];
 
 export default routes;
