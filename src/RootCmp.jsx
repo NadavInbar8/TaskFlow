9250841396e337513fe57a82b4cc437406d597bc
NadavInbar8 <nadavfrc@gmail.com>
2022-01-25 15:37:18 +0200

    added css layout

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,36 +1,46 @@
 import React from 'react';
-import {Switch, Route, useLocation} from 'react-router-dom';
-import {useSelector, useDispatch, shallowEqual} from 'react-redux';
+import { Switch, Route, useLocation } from 'react-router-dom';
+import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import routes from './routes.js';
-import {AppHeader} from './cmps/AppHeader.jsx';
+import { AppHeader } from './cmps/AppHeader.jsx';
 
 export function RootCmp() {
-	const {board} = useSelector((state) => ({board: state.boardModule.currBoard}), shallowEqual);
-	const location = useLocation();
+  const { board } = useSelector(
+    (state) => ({ board: state.boardModule.currBoard }),
+    shallowEqual
+  );
+  const location = useLocation();
 
-	const getbackgroundcolor = () => {
-		// return `${board.style.backgroundColor}`;
-		if (location.pathname === '/') return 'lightcyan';
-		else if (location.pathname.includes('/board') && board) return board.style.backgroundColor;
-	};
+  const getbackgroundcolor = () => {
+    // return `${board.style.backgroundColor}`;
+    if (location.pathname === '/') return 'lightcyan';
+    else if (location.pathname.includes('/board') && board)
+      return board.style.backgroundColor;
+  };
 
-	const getHeaderColor = () => {
-		if (location.pathname.includes('/board') && board) return 'rgba(0, 0, 0, 0.45)';
-		else if (location.pathname.includes('/workspace')) return '#026aa7';
-	};
+  const getHeaderColor = () => {
+    if (location.pathname.includes('/board') && board)
+      return 'rgba(0, 0, 0, 0.45)';
+    else if (location.pathname.includes('/workspace')) return '#026aa7';
+  };
 
-	return (
-		<div>
-			<main style={{backgroundColor: getbackgroundcolor()}}>
-				<AppHeader style={{backgroundColor: getHeaderColor()}} />
-				<Switch>
-					{routes.map((route) => (
-						<Route key={route.path} component={route.component} path={route.path} />
-					))}
-				</Switch>
-			</main>
-		</div>
-	);
+  return (
+    <main
+      className='flex-column'
+      style={{ backgroundColor: getbackgroundcolor() }}
+    >
+      <AppHeader style={{ backgroundColor: getHeaderColor() }} />
+      <Switch>
+        {routes.map((route) => (
+          <Route
+            key={route.path}
+            component={route.component}
+            path={route.path}
+          />
+        ))}
+      </Switch>
+    </main>
+  );
 }
 
 // function mapStateToProps({ toyModule }) {
