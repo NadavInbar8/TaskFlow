64e729cf4c4de250c240ff88da5365b232a487f2
Oded Kovo <odedkovo2@gmail.com>
2022-01-28 08:55:55 +0200

    fixed bug on adding board

--- src/pages/WorkSpace.jsx
+++ src/pages/WorkSpace.jsx
@@ -1,15 +1,15 @@
-import React, {useEffect, useState} from 'react';
-import {Link, useHistory, useLocation} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 
 // Redux
-import {useSelector, useDispatch, shallowEqual} from 'react-redux';
-import {loadBoards, openModal, updateBoard} from '../store/board.action.js';
+import { useSelector, useDispatch, shallowEqual } from 'react-redux';
+import { loadBoards, openModal, updateBoard } from '../store/board.action.js';
 
 // Services
-import {userService} from '../services/user.service.js';
+import { userService } from '../services/user.service.js';
 
 // Cmps
-import {CreateModal} from '../cmps/headerCmps/CreateModal.jsx';
+import { CreateModal } from '../cmps/headerCmps/CreateModal.jsx';
 
 // Images
 import boardPreview from '../assets/imgs/boardPreview.jpg';
@@ -20,77 +20,82 @@ import blackStar from '../assets/imgs/blackStar.svg';
 import logo from '../assets/imgs/favicon/taskflow-favicon.svg';
 
 export const WorkSpace = () => {
-	const history = useHistory();
-	const {boards} = useSelector((state) => ({
-		boards: state.boardModule.boards,
-	}));
-	const [starredBoards, setStarredBoards] = useState([]);
+  const history = useHistory();
+  const { boards } = useSelector((state) => ({
+    boards: state.boardModule.boards,
+  }));
+  const [starredBoards, setStarredBoards] = useState([]);
 
-	const {modal} = useSelector((state) => ({
-		modal: state.boardModule.modal,
-	}));
+  const { modal } = useSelector((state) => ({
+    modal: state.boardModule.modal,
+  }));
 
-	const dispatch = useDispatch();
-	const location = useLocation();
+  const dispatch = useDispatch();
+  const location = useLocation();
 
-	const loggedInUser = userService.getLoggedinUser();
+  const loggedInUser = userService.getLoggedinUser();
 
-	// console.log(loggedInUser);
-	if (loggedInUser === null) userService.connectGuestUser();
+  // console.log(loggedInUser);
+  if (loggedInUser === null) userService.connectGuestUser();
 
-	useEffect(() => {
-		dispatch(loadBoards());
-	}, []);
-	useEffect(() => {
-		getStarredBoards();
-	}, [boards]);
+  useEffect(() => {
+    dispatch(loadBoards());
+  }, []);
+  useEffect(() => {
+    getStarredBoards();
+  }, [boards]);
 
-	function getStarredBoards() {
-		const boardsStarred = boards.filter((board) => {
-			return board.starred === true;
-		});
-		setStarredBoards(boardsStarred);
-	}
-	function toggleModal(type) {
-		dispatch(openModal(type));
-	}
+  function getStarredBoards() {
+    const boardsStarred = boards.filter((board) => {
+      return board.starred === true;
+    });
+    setStarredBoards(boardsStarred);
+  }
+  function toggleModal(type) {
+    dispatch(openModal(type));
+  }
 
-	function starBoard(ev, board) {
-		ev.stopPropagation();
-		// console.log(board);
-		let newBoard = board;
-		if (newBoard.starred === true) {
-			newBoard.starred = false;
-		} else {
-			newBoard.starred = true;
-			// console.log(newBoard);
-		}
+  function starBoard(ev, board) {
+    ev.stopPropagation();
+    // console.log(board);
+    let newBoard = board;
+    if (newBoard.starred === true) {
+      newBoard.starred = false;
+    } else {
+      newBoard.starred = true;
+      // console.log(newBoard);
+    }
 
-		dispatch(updateBoard(newBoard));
-	}
+    dispatch(updateBoard(newBoard));
+  }
 
-	function connectUser() {
-		if (loggedInUser === null) userService.connectGuestUser();
-		else return;
-	}
+  function connectUser() {
+    if (loggedInUser === null) userService.connectGuestUser();
+    else return;
+  }
 
-	const getBackground = (board) => {
-		// return `${board.style.backgroundColor}`;
-		// if (location.pathname === '/') return 'lightcyan';
-		return board.style.userClicked ? board.style.backgroundColor : `url(${board.style.previewImgUrl})`;
-		// console.log(background);
-		// return background;
-	};
+  const getBackground = (board) => {
+    // return `${board.style.backgroundColor}`;
+    // if (location.pathname === '/') return 'lightcyan';
+    return board.style.userClicked
+      ? board.style.backgroundColor
+      : `url(${board.style.previewImgUrl})`;
+    // console.log(background);
+    // return background;
+  };
 
-	return (
-		<div className='work-space'>
-			{/* {console.log('render')} */}
-			<div className='boards'>
-				<h2 className='flex flex-center'>
-					<img style={{height: '30px', paddingRight: '20px'}} src={blackStar} />
-					Starred boards:
-				</h2>
-				{/* <div className='star-boards-container'>
+  return (
+    <div className='work-space'>
+      {/* {console.log('render')} */}
+      <div className='boards'>
+        <h2 className='flex flex-center'>
+          <img
+            style={{ height: '30px', paddingRight: '20px' }}
+            src={blackStar}
+          />
+          Starred boards:
+        </h2>
+        {/* <div className='star-boards-container'>
 					{starredBoards &&
 						starredBoards.map((starredBoard, idx) => {
 							return (
@@ -125,59 +130,62 @@ export const WorkSpace = () => {
 						})}
 				</div> */}
 
-				<div className='boards-container'>
-					{starredBoards &&
-						starredBoards.map((starredBoard, idx) => {
-							return (
-								<div className='preview-container'>
-									<div
-										key={idx}
-										// onClick={() => history.push(`/board/${board._id}`)}
-										className='board-preview'>
-										<div
-											style={
-												starredBoard?.style?.userClicked
-													? {backgroundColor: getBackground(starredBoard)}
-													: {backgroundImage: getBackground(starredBoard)}
-											}
-											className='board-background-div preview-cover'>
-											{starredBoard.style?.backgroundColor && (
-												<div
-													onClick={() => {
-														connectUser();
-														history.push(`/board/${starredBoard._id}`);
-													}}
-													className='board-title-div'
-													// style={{ backgroundColor: board.style.backgroundColor }}
-												>
-													{console.log(starredBoard)}
-													<h3 className='workspace-board-title'>{starredBoard.title}</h3>
-													<div className='star-svg'>
-														{starredBoard.starred === false && (
-															<img
-																onClick={(ev) => {
-																	starBoard(ev, starredBoard);
-																}}
-																className='star-svg-img'
-																src={star}
-																alt=''
-															/>
-														)}
-														{starredBoard.starred === true && (
-															<img
-																onClick={(ev) => {
-																	starBoard(ev, starredBoard);
-																}}
-																className='star-svg-img'
-																src={goldStar}
-																alt=''
-															/>
-														)}
-													</div>
-												</div>
-											)}
-										</div>
-										{/* {!starredBoard.style?.backgroundColor && (
+        <div className='boards-container'>
+          {starredBoards &&
+            starredBoards.map((starredBoard, idx) => {
+              return (
+                <div key={idx} className='preview-container'>
+                  <div
+                    // onClick={() => history.push(`/board/${board._id}`)}
+                    className='board-preview'
+                  >
+                    <div
+                      style={
+                        starredBoard?.style?.userClicked
+                          ? { backgroundColor: getBackground(starredBoard) }
+                          : { backgroundImage: getBackground(starredBoard) }
+                      }
+                      className='board-background-div preview-cover'
+                    >
+                      {starredBoard.style?.backgroundColor && (
+                        <div
+                          onClick={() => {
+                            connectUser();
+                            history.push(`/board/${starredBoard._id}`);
+                          }}
+                          className='board-title-div'
+                          // style={{ backgroundColor: board.style.backgroundColor }}
+                        >
+                          {/* {console.log(starredBoard)} */}
+                          <h3 className='workspace-board-title'>
+                            {starredBoard.title}
+                          </h3>
+                          <div className='star-svg'>
+                            {starredBoard.starred === false && (
+                              <img
+                                onClick={(ev) => {
+                                  starBoard(ev, starredBoard);
+                                }}
+                                className='star-svg-img'
+                                src={star}
+                                alt=''
+                              />
+                            )}
+                            {starredBoard.starred === true && (
+                              <img
+                                onClick={(ev) => {
+                                  starBoard(ev, starredBoard);
+                                }}
+                                className='star-svg-img'
+                                src={goldStar}
+                                alt=''
+                              />
+                            )}
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                    {/* {!starredBoard.style?.backgroundColor && (
 											<div>
 												<img src={boardPreview} alt='' />
 												<div className='star-svg'>
@@ -185,94 +193,98 @@ export const WorkSpace = () => {
 												</div>
 											</div>
 										)} */}
-									</div>
-								</div>
-							);
-						})}
-				</div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
 
-				<h2 className='flex flex-center'>
-					<img className='workspace-logo' src={logo} />
-					Work Space:
-				</h2>
-				<div className='boards-container'>
-					{boards.length > 0 &&
-						boards.map((board, idx) => {
-							console.log(board);
-							return (
-								<div className='preview-container'>
-									<div
-										key={idx}
-										// onClick={() => history.push(`/board/${board._id}`)}
-										className='board-preview'>
-										<div
-											style={
-												board?.style?.userClicked
-													? {backgroundColor: getBackground(board)}
-													: {backgroundImage: getBackground(board)}
-											}
-											className='board-background-div preview-cover'>
-											{board.style?.backgroundColor && (
-												<div
-													onClick={() => {
-														connectUser();
-														history.push(`/board/${board._id}`);
-													}}
-													className='board-title-div'
-													// style={{ backgroundColor: board.style.backgroundColor }}
-												>
-													{console.log(board)}
-													<h3 className='workspace-board-title'>{board.title}</h3>
-													<div className='star-svg'>
-														{board.starred === false && (
-															<img
-																onClick={(ev) => {
-																	starBoard(ev, board);
-																}}
-																className='star-svg-img'
-																src={star}
-																alt=''
-															/>
-														)}
+        <h2 className='flex flex-center'>
+          <img className='workspace-logo' src={logo} />
+          Work Space:
+        </h2>
+        <div className='boards-container'>
+          {boards.length > 0 &&
+            boards.map((board, idx) => {
+              //   console.log(board);
+              return (
+                <div key={idx} className='preview-container'>
+                  <div
+                    // onClick={() => history.push(`/board/${board._id}`)}
+                    className='board-preview'
+                  >
+                    <div
+                      style={
+                        board?.style?.userClicked
+                          ? { backgroundColor: getBackground(board) }
+                          : { backgroundImage: getBackground(board) }
+                      }
+                      className='board-background-div preview-cover'
+                    >
+                      {board.style?.backgroundColor && (
+                        <div
+                          onClick={() => {
+                            connectUser();
+                            history.push(`/board/${board._id}`);
+                          }}
+                          className='board-title-div'
+                          // style={{ backgroundColor: board.style.backgroundColor }}
+                        >
+                          {/* {console.log(board)} */}
+                          <h3 className='workspace-board-title'>
+                            {board.title}
+                          </h3>
+                          <div className='star-svg'>
+                            {board.starred === false && (
+                              <img
+                                onClick={(ev) => {
+                                  starBoard(ev, board);
+                                }}
+                                className='star-svg-img'
+                                src={star}
+                                alt=''
+                              />
+                            )}
 
-														{board.starred === true && (
-															<img
-																onClick={(ev) => {
-																	starBoard(ev, board);
-																}}
-																className='star-svg-img'
-																src={goldStar}
-																alt=''
-															/>
-														)}
-													</div>
-												</div>
-											)}
-										</div>
-										{!board.style?.backgroundColor && (
-											<div>
-												<img src={boardPreview} alt='' />
-												<div className='star-svg'>
-													<img src={star} alt='' />
-												</div>
-											</div>
-										)}
-									</div>
-								</div>
-							);
-						})}
-					<div
-						onClick={() => {
-							toggleModal('createModal');
-						}}
-						className='add-board-div'>
-						<h3>Add Board</h3>
-						{modal === 'createModal2' && <CreateModal parentCmp='workspace' />}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+                            {board.starred === true && (
+                              <img
+                                onClick={(ev) => {
+                                  starBoard(ev, board);
+                                }}
+                                className='star-svg-img'
+                                src={goldStar}
+                                alt=''
+                              />
+                            )}
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                    {!board.style?.backgroundColor && (
+                      <div>
+                        <img src={boardPreview} alt='' />
+                        <div className='star-svg'>
+                          <img src={star} alt='' />
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              );
+            })}
+          <div
+            onClick={() => {
+              toggleModal('createModal');
+            }}
+            className='add-board-div'
+          >
+            <h3>Add Board</h3>
+            {modal === 'createModal2' && <CreateModal parentCmp='workspace' />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 // function mapStateToProps({ boardModule }) {
